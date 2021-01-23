c6dbfbe92b75c3b2941ad5711ed40ed3440b3afb
Andrii Yerko <aerko12@gmail.com>
2021-01-23 23:57:22 +0200

    pelmen'

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return 'Under construction!'
+    return render_template('example.html')
+
+@app.route('/styles/styless.css')
+def style():
+    return send_from_directory('styles','styless.css')
+
+@app.route('/images/pel.jpg')
+def picture():
+    return send_from_directory('images','pel.jpg')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+      host='0.0.0.0',
+      port=2000
+      )
