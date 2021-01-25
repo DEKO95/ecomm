e075d90d30673e459861902f90344ad2692e8436
Andrii Yerko <aerko12@gmail.com>
2021-01-25 23:37:16 +0200

    Introduce jinja templates and preparing for DB

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 app = Flask(__name__, root_path=os.getcwd())
+# a little template for the future
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
 from server import routes
--- server/routes.py
+++ server/routes.py
@@ -4,8 +4,12 @@ from flask import render_template, send_from_directory
 
 @app.route('/')
 def main_page():
-    return render_template('example.html')
+    return render_template('index.html')
 
+@app.route('/developers')
+def developers():
+    return render_template('developers.html')
+    
 @app.route('/styles/styless.css')
 def style():
     return send_from_directory('styles','styless.css')
