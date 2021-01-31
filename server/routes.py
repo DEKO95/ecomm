855c1ae33f22b50059ae4cf2cc057d7c98f139a9
Andrii Yerko <aerko12@gmail.com>
2021-01-31 14:53:44 +0200

    Remove rudimentary files

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,3 @@
-from deprecated import deprecated
 from flask import render_template, send_from_directory
 
 from server import app
@@ -6,16 +5,4 @@ from server import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@deprecated(reason="Will be removed after switching to the correct SPA model")
-@app.route('/styles/styless.css')
-def style():
-    return send_from_directory('styles','styless.css')
-
-
-@deprecated(reason="Will be removed after switching to the correct SPA model")
-@app.route('/images/<img>')
-def picture(img):
-    return send_from_directory('images',img)
+    return """<h1> Under construction </h1>"""
